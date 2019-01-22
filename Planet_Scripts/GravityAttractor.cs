77f4e0b9c398959d5d65e0b2f389659b9786983a
unknown <Alfie_Sargent@hotmail.com>
2019-01-22 23:51:13 +0000

    Sorted out the comments a bit...

--- Planet_Scripts/GravityAttractor.cs
+++ Planet_Scripts/GravityAttractor.cs
@@ -2,17 +2,22 @@
 using System.Collections;
 
 public class GravityAttractor : MonoBehaviour {
+
+    //APPLY THIS SCRIPT TO THE PLANETARY OBJECTS
+    //This script is responsible for the gravitational forces felt by different planets.
 	
+    //a float for gravitational force. Public so that it can be changed for different planets.
 	public float gravity = -9.8f;
-	
-	
-	public void Attract(Rigidbody body) {
-		Vector3 gravityUp = (body.position - transform.position).normalized;
-		Vector3 localUp = body.transform.up;
-		
-		// Apply downwards gravity to body
-		body.AddForce(gravityUp * gravity);
-		// Allign bodies up axis with the centre of planet
-		body.rotation = Quaternion.FromToRotation(localUp,gravityUp) * body.rotation;
-	}  
+
+    public void Attract(Rigidbody body)
+    {
+        //getting a direction for the body that is normalized so that it is always the same length.
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 localUp = body.transform.up;
+
+        // Actually applying the graviational force to the body.
+        body.AddForce(gravityUp * gravity);
+        // Making sure that the bodies that are being affected are alligned with the centre of the planet.
+        body.rotation = Quaternion.FromToRotation(localUp, gravityUp) * body.rotation;
+    }
 }
--- Planet_Scripts/GravityBody.cs
+++ Planet_Scripts/GravityBody.cs
@@ -1,23 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+//APPLY THIS SCRUPT TO ALL OBJECTS THAT YOU WANT TO BE EFFECTED BY THE GRAVITY ATTRACTOR
+//it must always have a rigidbody attached or else it wont work...it is required.
+
 [RequireComponent (typeof (Rigidbody))]
 public class GravityBody : MonoBehaviour {
 	
+    //referencing the GravityAttractor script.
 	GravityAttractor planet;
 	Rigidbody rigidbody;
 	
 	void Awake () {
+        //Started to use tags, they are the best.
 		planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravityAttractor>();
 		rigidbody = GetComponent<Rigidbody> ();
 
-		// Disable rigidbody gravity and rotation as this is simulated in GravityAttractor script
+		// We have our own gravity...kind of the whole point. Disabling the in built gravity is required.
 		rigidbody.useGravity = false;
+        //Same as before, we want to do our own rotation.
 		rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
 	}
 	
 	void FixedUpdate () {
-		// Allow this body to be influenced by planet's gravity
+		// Basically allowing it to be transformed. 
 		planet.Attract(rigidbody);
 	}
 }
